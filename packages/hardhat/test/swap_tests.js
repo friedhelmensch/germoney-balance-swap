a8f11e180449e61e16491c991a900aa26c96c76e
Hannes <hannes@rang.de>
2022-02-23 20:07:08 +0100

    deploying swap for test

--- packages/hardhat/test/swap_tests.js
+++ packages/hardhat/test/swap_tests.js
@@ -4,27 +4,35 @@ const { solidity } = require("ethereum-waffle");
 
 use(solidity);
 
-describe("My Dapp", function () {
-  let myContract;
+describe("Germoney-Balance-Swap", function () {
+  let swapContract;
 
   // quick fix to let gas reporter fetch data from gas station & coinmarketcap
   before((done) => {
     setTimeout(done, 2000);
   });
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+  describe("Swap", function () {
+    it("Should deploy Swap", async function () {
+      const Swap = await ethers.getContractFactory("Swap");
 
-      myContract = await YourContract.deploy("My purpose");
+      const germoney = await (
+        await ethers.getContractFactory("Germoney")
+      ).deploy();
+
+      const balance = await (
+        await ethers.getContractFactory("Balance")
+      ).deploy();
+
+      swapContract = await Swap.deploy(germoney.address, balance.address);
     });
 
     describe("setPurpose()", function () {
       it("Should be able to set a new purpose", async function () {
         const newPurpose = "Test Purpose";
 
-        await myContract.swap(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
+        await swapContract.swap(newPurpose);
+        expect(await swapContract.purpose()).to.equal(newPurpose);
       });
 
       // Uncomment the event and emit lines in YourContract.sol to make this test pass
