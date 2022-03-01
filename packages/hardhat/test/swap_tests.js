113aefc6db117ebef7f19912a3e6720d047b946c
Hannes <hannes@rang.de>
2022-03-01 19:59:50 +0100

    restructuring test for readability

--- packages/hardhat/test/swap_tests.js
+++ packages/hardhat/test/swap_tests.js
@@ -26,18 +26,19 @@ describe("Germoney-Balance-Swap", function () {
     describe("swap", function () {
       it("should send balance to swapper", async function () {
         await balance.transfer(swap.address, 50);
-
         const swapBalanceTokens = await balance.balanceOf(swap.address);
         expect(swapBalanceTokens).to.equal(50);
 
         const [owner] = await ethers.getSigners();
-        const balanceTokens = await balance.balanceOf(owner.address);
-        expect(balanceTokens).to.equal(50);
+        const ownerBalanceTokens = await balance.balanceOf(owner.address);
+        expect(ownerBalanceTokens).to.equal(50);
 
         await swap.swap(50);
 
-        const balanceTokensAfterSwap = await balance.balanceOf(owner.address);
-        expect(balanceTokensAfterSwap).to.equal(100);
+        const ownerBalanceTokensAfterSwap = await balance.balanceOf(
+          owner.address
+        );
+        expect(ownerBalanceTokensAfterSwap).to.equal(100);
       });
 
       // Uncomment the event and emit lines in YourContract.sol to make this test pass
